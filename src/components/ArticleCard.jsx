23b427c6aa2b1482229807a0d640046d6c4321fd
Julian22222 <julik.golovenj@gmail.com>
2025-06-13 22:07:56 +0100

    gif

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -5,7 +5,7 @@ const ArticleCard = (props) => {
   const date = moment(props.created_at).format("DD/MM/YYYY");
   const time = moment(props.created_at).format("hh:mm A");
   return (
-    <div className="Card">
+    <div className="Card" style={{ position: "relative", height: "310px" }}>
       <div className="article-head-container">
         <div className="left">
           <p className="ArticleID">Article Id: {props.article_id}</p>
@@ -22,9 +22,11 @@ const ArticleCard = (props) => {
 
       <p>Votes: {props.votes}</p>
       <p>Comment count: {props.comment_count}</p>
-      <Link to={`/articles/${props.article_id}`} className="btnReadArticle">
-        Read article
-      </Link>
+      <div style={{ position: "absolute", bottom: "10px", right: "10px" }}>
+        <Link to={`/articles/${props.article_id}`} className="btnReadArticle">
+          Read article
+        </Link>
+      </div>
     </div>
   );
 };
